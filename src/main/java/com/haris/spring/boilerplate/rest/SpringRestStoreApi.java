b4d631c344c23cf869a9e86b7e51407fbd616f4b
mharis05 <harissaleemkhi@gmail.com>
2019-10-19 20:14:12 +0200

    Added simple implementation of DELETE employee and fixed some config issues with mySQL connection

--- src/main/java/com/haris/spring/boilerplate/rest/SpringRestStoreApi.java
+++ src/main/java/com/haris/spring/boilerplate/rest/SpringRestStoreApi.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringRestStoreApi {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRestStoreApi.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestStoreApi.class, args);
+    }
 
 }
--- src/main/java/com/haris/spring/boilerplate/rest/service/EmployeeService.java
+++ src/main/java/com/haris/spring/boilerplate/rest/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.haris.spring.boilerplate.rest.service;
 
 import com.haris.spring.boilerplate.rest.model.Employee;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -8,4 +9,5 @@ public interface EmployeeService {
     public abstract void createEmployee(Employee employee);
     public abstract List<Employee> getEmployees();
     public abstract Employee getEmployee(Integer code);
+    public abstract ResponseEntity<String> deleteEmployee(Integer code);
 }
